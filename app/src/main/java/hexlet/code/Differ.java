32c410e39d08b7e5f7f94f356721cc964f7acdfb
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-04-01 14:25:43 +0300

    изменение метода build

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.LinkedHashMap;
 
 
 import static hexlet.code.Formatter.chooseFormat;
@@ -25,21 +31,18 @@ public class Differ {
         for (var key :keys) {
             Object value1 = file1.get(key) == null ? "null" : file1.get(key);
             Object value2 = file2.get(key) == null ? "null" : file2.get(key);
-            if (file2.containsKey(key) && value1.equals(value2)) {
+            if (Objects.equals(file1.get(key), (file2.get(key)))) {
                 Map<String, Object> node = Map.of("type", "unchanged", "key", key, "newValue", value1);
                 result.add(node);
-            }
-            if (file2.containsKey(key) && !value1.equals(value2)) {
-                Map<String, Object> node = Map.of("type", "changed", "key", key, "oldValue",
-                        value1, "newValue", value2);
-                result.add(node);
-            }
-            if (!file2.containsKey(key)) {
+            } else if (file1.containsKey(key) && !file2.containsKey(key)) {
                 Map<String, Object> node = Map.of("type", "deleted", "key", key, "newValue", value1);
                 result.add(node);
-            }
-            if (!file1.containsKey(key)) {
+            } else if (file2.containsKey(key) && !file1.containsKey(key)) {
                 Map<String, Object> node = Map.of("type", "added", "key", key, "newValue", value2);
+                result.add(node);
+            } else {
+                Map<String, Object> node = Map.of("type", "changed", "key", key, "oldValue",
+                        value1, "newValue", value2);
                 result.add(node);
             }
         }
